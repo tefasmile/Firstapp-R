c1d1e846cf9e5f6e2ee493e504709a6755d6dd05
tefasmile <tefaz.n@gmail.com>
2019-07-17 20:37:51 -0500

    Adicionando la pagina de Badges.js a la app

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
 import BadgeNew from './pages/BadgeNew';
+import Badges from './pages/Badges';
 import Badge from './components/Badge';
 
 
 
 const container = document.getElementById('app');
-ReactDOM.render(<BadgeNew />, 
+ReactDOM.render(<Badges />, 
     container
 );
 
