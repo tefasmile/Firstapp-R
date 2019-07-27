84de8863c4c9d2e04b167268090bb59a33ffc5f9
tefasmile <tefaz.n@gmail.com>
2019-07-26 19:40:50 -0500

    Se han creado las rutas dentro de nuestro BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import Layout from './Layout';
-import Home from '../pages/Home';
-import Badges from '../pages/Badges';
+//agregamos nuestras paginas de la app
 import BadgeNew from '../pages/BadgeNew';
-import NotFound from '../pages/NotFound';
+import Badges from '../pages/Badges';
+
 
 function App() {
-  return (
+  return(
     <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/badges" component={Badges} />
-          <Route exact path="/badges/new" component={BadgeNew} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
+      <div>
+        <Route path="/badges" component={Badges} />
+        <Route path="/badges/new" component={BadgeNew} />
+      </div>
     </BrowserRouter>
   );
 }
 
+
 export default App;
+
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// import Layout from './Layout';
+// import Home from '../pages/Home';
+// import Badges from '../pages/Badges';
+// import BadgeNew from '../pages/BadgeNew';
+// import NotFound from '../pages/NotFound';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Layout>
+//         <Switch>
+//           <Route exact path="/" component={Home} />
+//           <Route exact path="/badges" component={Badges} />
+//           <Route exact path="/badges/new" component={BadgeNew} />
+//           <Route component={NotFound} />
+//         </Switch>
+//       </Layout>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
-import BadgeNew from './pages/BadgeNew';
-import Badges from './pages/Badges';
-import Badge from './components/Badge';
+//import Badge from './components/Badge';
 import App from './components/App';//aqui pondremos el router domo con sus rutas
 
 
 
 const container = document.getElementById('app');
-ReactDOM.render(<Badges />, 
+ReactDOM.render(<App />, 
     container
 );
 
